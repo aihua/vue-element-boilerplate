aa5574a293685f60f99758eab8a3c0cd4f16e795
LeonWong <wangliran@bluemoon.com.cn>
2017-08-19 12:03:25 +0800

    整理路由

---
+++ src/core/index.js
@@ -0,0 +1,12 @@
+/* Components */
+import Vue from 'vue';
+import PageContainer from '../components/PageContainer';
+
+/* Filters */
+import Capitalize from './filters/Capitalize/Capitalize';
+import Uppercase from './filters/Uppercase/Uppercase';
+
+Vue.component(PageContainer.name, PageContainer);
+
+Vue.filter('capitalize', Capitalize);
+Vue.filter('uppercase', Uppercase);
