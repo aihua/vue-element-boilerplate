f79a501f92d754174b1e719b8bb0e1fbe0cd990e
LeonWong <wangliran@bluemoon.com.cn>
2017-11-04 11:40:28 +0800

    修复未登录 bug

--- src/pages/Entrance.vue
+++ src/pages/Entrance.vue
@@ -31,6 +31,22 @@ export default {
       this.$store.commit('module/' + types.SET_CURR_ENTRY_NAME, entryName);
       this.$router.push(url);
     }
+  },
+  mounted() {
+    try {
+      let token = this.$store.getters['account/getToken'];
+
+      this.$assert.notEqual(token, '', '用户未登录');
+      let payload = this.$base64url.decode(token.split('.')[1]);
+
+      if (JSON.parse(payload).exp < new Date().getTime()) {
+        this.store.clear();
+        this.$router.push('/login');
+      }
+    } catch (error) {
+      this.$message.warning('检测到你尚未登录，请登录哦^_^');
+      this.$router.push('/login');
+    }
   }
 };
 </script>
